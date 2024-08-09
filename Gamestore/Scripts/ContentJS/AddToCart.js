8e1da70919bd349fd80d754c86b60a8e8825f5dd
LFMarlow <122919718+LFMarlow@users.noreply.github.com>
2024-08-09 17:49:00 +0200

    Changement Panier JS

---
+++ Gamestore/Scripts/ContentJS/AddToCart.js
@@ -0,0 +1,16 @@
+﻿function addToCart(title) {
+    $.ajax({
+        type: "POST",
+        url: "Jeuxvideo.aspx/AddToCart",
+        data: JSON.stringify({ title: title }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            alert(response.d);
+            location.reload();
+        },
+        error: function (response) {
+
+        }
+    });
+}
